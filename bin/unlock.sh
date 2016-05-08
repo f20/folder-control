f965ce842feddec53707397c9abfbc615f5372eb
FL <f20@reckon.co.uk>
2016-05-08 10:36:15 +0000

    Fix.

--- bin/unlock.sh
+++ bin/unlock.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 if [ "$*" != "" ]
 then
-	find "$@" -type f -not -perm +0220 -not -name .DS_Store | while read x
+	find "$@" -type f -not -perm +020 -not -name .DS_Store | while read x
 	do
 		cat "$x" > "$x$$"
+		chmod g+w "$x$$"
 		if [ -x "$x" ]; then chmod +x "$x$$"; fi
 		mv -f "$x$$" "$x"
 	done
