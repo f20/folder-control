83260dca97563c75a8e2dd16708a23c9126248d2
FL <f20@reckon.co.uk>
2016-05-22 13:44:13 +0000

    Bug fix in unlock script.

--- bin/unlock.sh
+++ bin/unlock.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 if [ "$*" != "" ]
 then
-	find "$@" -type f -links 1 -not -perm +020 -not -name .DS_Store -exec chmod g+w {} \;
+	find "$@" -type f -links 1 -not -perm +020 -not -name .DS_Store -exec chmod ug+w {} \;
 	find "$@" -type f -not -perm +020 -not -name .DS_Store | while read x
 	do	
 		echo $x
 		cat "$x" > "$x$$"
-		chmod g+w "$x$$"
+		chmod ug+w "$x$$"
 		if [ -x "$x" ]; then chmod +x "$x$$"; fi
 		mv -f "$x$$" "$x"
 	done
